75a3a3e070376bd8d209ba02a45ac2425d1ece4a
Krzysztof Szukiełojć <kszukielojc@mirantis.com>
2017-04-25 11:30:10 +0200

    Adding running parameter to stats
    This change is intended to give information in metrics
    if our process is not running or we are missing just a
    metrics from it.
    
    Change-Id: Ic1e729206fad70cc681af09222ef47234a0d4d37

--- plugins/inputs/procstat/procstat.go
+++ plugins/inputs/procstat/procstat.go
@@ -71,6 +71,15 @@ func (p *Procstat) Gather(acc telegraf.Accumulator) error {
 
 	procs, err := p.updateProcesses(p.procs)
 	if err != nil {
+		if p.ProcessName != "" {
+			var prefix string
+			if p.Prefix != "" {
+				prefix = p.Prefix + "_"
+			}
+			tags := map[string]string{"process_name": p.ProcessName}
+			fields := map[string]interface{}{prefix + "running": 0}
+			acc.AddFields("procstat", fields, tags)
+		}
 		acc.AddError(fmt.Errorf("E! Error: procstat getting process, exe: [%s] pidfile: [%s] pattern: [%s] user: [%s] %s",
 			p.Exe, p.PidFile, p.Pattern, p.User, err.Error()))
 	}
